ab77c3f9c1aac1e7af193444789f953275fec6e4
Meshack Bwire <b.meshack@outlook.com>
2024-03-27 01:19:36 -0700

    pp apply

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# Using Puppet to create a file in /tmp.
+# Puppet manifest to create a file in /tmp
 file { '/tmp/school':
-    ensure  => 'present',
-    content => 'I love Puppet',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744'
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
+
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Using Puppet to install flask from pip3.
+# Puppet manifest to install Flask package
 package { 'flask':
-  ensure   => '2.1.0',
+  ensure => '2.1.0',
   provider => 'pip3',
 }
+
